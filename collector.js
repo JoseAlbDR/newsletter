7d6d53b28a29b37dfb57bb6c44e9ef00cc9b2623
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-27 11:12:18 +0200

    Reestructured dirs, added API_KEY

--- collector.js
+++ collector.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const api = require("./private/api.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,13 +10,16 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.post("/", (req, res) => {
   console.log(req.body);
-
+  const API_KEY = api.APY_KEY;
   const { name } = req.body;
   const { email } = req.body;
-  res.write(`Your name is: ${name} and your email is ${email}`);
+  const { lastName } = req.body;
+  console.log(api);
+
+  res.write(`Your name is: ${name} ${lastName} and your email is ${email}.`);
   res.send();
 });
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000, () => console.log("Server started on port: 3000"));
---
+++ private/api.js
@@ -0,0 +1,3 @@
+module.exports = {
+  APY_KEY: "e74523b3c2ad6904bd69ac666554db61-us8",
+};
