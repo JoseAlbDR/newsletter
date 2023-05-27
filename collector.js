8eab712cc3173795d173ac3e03809eb63d8a32b3
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-27 12:26:36 +0200

    Succesfully added mailchimp API call

--- collector.js
+++ collector.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const api = require("./private/api");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,12 +12,58 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.post("/", (req, res) => {
   console.log(req.body);
+  const LIST_ID = api.AUDIENCE_ID;
+  const API_KEY = api.API_KEY; // Your mailchimp API_KEY goes here
+  const { name, lastName, email } = req.body;
+  const url = `https://us8.api.mailchimp.com/3.0/lists/${LIST_ID}`;
+  const options = { method: "POST", auth: `yusep:${API_KEY}` };
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: name,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
-  const API_KEY = api.API_KEY;
-  const { name } = req.body;
-  const { email } = req.body;
-  res.write(`Your name is: ${name} and your email is ${email} ${API_KEY}`);
-  res.send();
+  client.setConfig({
+    apiKey: API_KEY,
+    server: "us8",
+  });
+
+  const run = async () => {
+    const response = await client.lists.batchListMembers(LIST_ID, {
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: name,
+            LNAME: lastName,
+          },
+        },
+      ],
+    });
+    console.log(response);
+  };
+
+  run();
+  // const jsonData = JSON.stringify(data);
+
+  // const request = https.request(url, options, (response) => {
+  //   console.log(
+  //     response.on("data", (data) => {
+  //       console.log(JSON.parse(data));
+  //     })
+  //   );
+  // });
+
+  // request.write(jsonData);
+  // request.end();
 });
 
 app.use(express.static(__dirname + "/"));
